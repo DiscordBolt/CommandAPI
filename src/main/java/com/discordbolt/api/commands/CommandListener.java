5e6004163a992e7eb36b4ef1ff1aa54bb45b3a60
Tony Pappas <ajpappas@ncsu.edu>
2018-06-15 14:33:43 -0400

    Add print statements on command

--- src/main/java/com/discordbolt/api/commands/CommandListener.java
+++ src/main/java/com/discordbolt/api/commands/CommandListener.java
@@ -3,11 +3,15 @@ package com.discordbolt.api.commands;
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 import java.util.Objects;
 
 public class CommandListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandListener.class);
 
     private CommandManager manager;
 
@@ -25,7 +29,7 @@ public class CommandListener {
 
     private void onCommand(Message message) {
         Mono.just(message)
-            .filterWhen(msg -> msg.getGuild().map(manager::getCommandPrefix).map(prefix -> msg.getContent().get().startsWith(prefix)))
+            .filterWhen(msg -> msg.getGuild().map(manager::getCommandPrefix).map(prefix -> msg.getContent().get().startsWith(prefix))).doOnNext(thingy -> LOGGER.info(thingy.getContent().get()))
             .map(msg -> msg.getGuild()
                            .map(manager::getCommandPrefix)
                            .map(prefix -> msg.getContent().get().substring(prefix.length()))
@@ -35,7 +39,7 @@ public class CommandListener {
                                                      .filter(command -> matches(command, message.getContent().get()))
                                                      .reduce((first, second) -> second)
                                                      .orElse(null))
-                           .filter(Objects::nonNull)
+                           .filter(Objects::nonNull).doOnNext(thingy -> LOGGER.info(thingy.getCommands().toString()))
                            .doOnNext(command -> command.preexec(message)))
             .subscribe();
     }
